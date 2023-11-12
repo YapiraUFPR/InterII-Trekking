5031ee0575534e7d8ac3f3618a1ab25a63a099a0
gab <gabriel.pontarolo045@gmail.com>
2023-11-11 23:10:59 -0300

    update imu pub with new custom message

--- src/sensor_drivers/sensor_drivers/imu_publisher.py
+++ src/sensor_drivers/sensor_drivers/imu_publisher.py
@@ -41,35 +41,41 @@ def main():
     bno.enable_feature(BNO_REPORT_ACCELEROMETER)
     bno.enable_feature(BNO_REPORT_GYROSCOPE)
     bno.enable_feature(BNO_REPORT_MAGNETOMETER)
+    bno.enable_feature(BNO_REPORT_ROTATION_VECTOR)
     sleep(0.5)  # ensure IMU is initialized
 
     # main loop
     logger.info("Publishing IMU data...")
     while True:
-        raw_msg = Imu9DOF()
-        raw_msg.header.stamp = node.get_clock().now().to_msg()
+        imu_msg = Imu9DOF()
+        imu_msg.header.stamp = node.get_clock().now().to_msg()
 
-        accel_x, accel_y, accel_z = bno.acceleration
-        raw_msg.linear_acceleration.x = accel_x
-        raw_msg.linear_acceleration.y = accel_y
-        raw_msg.linear_acceleration.z = accel_z
+        quat_i, quat_j, quat_k, quat_real = bno.quaternion
+        imu_msg.orientation.x = quat_i
+        imu_msg.orientation.y = quat_j
+        imu_msg.orientation.z = quat_k
+        imu_msg.orientation.w = quat_real
+        imu_msg.orientation_covariance[0] = -1
 
         gyro_x, gyro_y, gyro_z = bno.gyro
-        raw_msg.angular_velocity.x = gyro_x
-        raw_msg.angular_velocity.y = gyro_y
-        raw_msg.angular_velocity.z = gyro_z
+        imu_msg.angular_velocity.x = gyro_x
+        imu_msg.angular_velocity.y = gyro_y
+        imu_msg.angular_velocity.z = gyro_z
+        imu_msg.angular_velocity_covariance[0] = -1
 
-        raw_msg.orientation_covariance[0] = -1
-        raw_msg.linear_acceleration_covariance[0] = -1
-        raw_msg.angular_velocity_covariance[0] = -1
+        accel_x, accel_y, accel_z = bno.acceleration
+        imu_msg.linear_acceleration.x = accel_x
+        imu_msg.linear_acceleration.y = accel_y
+        imu_msg.linear_acceleration.z = accel_z
+        imu_msg.linear_acceleration_covariance[0] = -1
 
         mag_x, mag_y, mag_z = bno.magnetic
-        raw_msg.magnetic_field.x = mag_x
-        raw_msg.magnetic_field.y = mag_y
-        raw_msg.magnetic_field.z = mag_z
-        raw_msg.magnetic_field_covariance[0] = -1
+        imu_msg.magnetic_field.x = mag_x
+        imu_msg.magnetic_field.y = mag_y
+        imu_msg.magnetic_field.z = mag_z
+        imu_msg.magnetic_field_covariance[0] = -1
         
-        imu_pub.publish(raw_msg)
+        imu_pub.publish(imu_msg)
 
         rate.sleep()
 
