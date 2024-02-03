0305b7fc6248e28acc80e868c6d8cb0eb3691dfa
Gabp0 <gabriel.pontarolo045@gmail.com>
2024-02-03 15:50:57 -0300

    update dockerfile

--- run_container.sh
+++ run_container.sh
@@ -1,2 +1,2 @@
 sudo systemctl start docker
-docker run --rm --name bedman --privileged -it --gpus all --network="host" -v $PWD:/home/user/ws/ -v /tmp.X11-unix:/tmp/.X11-unix -v /dev:/dev -e DISPLAY=$DISPLAY -w /home/user/ws/ --volume="$HOME/.Xauthority:/root/.Xauthority:rw" yapira/bedman:latest bash
+sudo docker run --rm --name bedman --privileged -it --gpus all --network="host" -v $PWD:/home/user/ws/ -v /tmp.X11-unix:/tmp/.X11-unix -v /dev:/dev -e DISPLAY=$DISPLAY -w /home/user/ws/ --volume="$HOME/.Xauthority:/root/.Xauthority:rw" yapira/bedman:latest bash
