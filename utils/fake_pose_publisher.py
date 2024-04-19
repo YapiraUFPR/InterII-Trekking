7e454d3007b5c4a21d014a6c8aab17285fb17340
gab <gabriel.pontarolo045@gmail.com>
2024-04-18 21:19:31 -0300

    Add fake pose publisher for tests.

---
+++ utils/fake_pose_publisher.py
@@ -0,0 +1,57 @@
+import rclpy
+from rclpy.node import Node
+from geometry_msgs.msg import PoseWithCovarianceStamped
+import numpy as np
+
+TOPIC = "/fake_pose"
+FREQUENCY = 30
+
+class FakePosePublisher(Node):
+
+    def __init__(self):
+        super().__init__("fake_pose_publisher")
+
+        self.logger = self.get_logger()
+        self.logger.info("Starting fake pose publisher...")
+        
+        self.curr_pose = np.array([0.0, 0.0, 0.0]) # in 3D
+
+        self.publisher = self.create_publisher(PoseWithCovarianceStamped, "/fake_pose", 10)
+        self.timer = self.create_timer(1/FREQUENCY, self.timer_callback)
+
+        self.logger.info("Fake pose publisher started.")
+
+    def timer_callback(self):
+
+        self.logger.info("Publishing fake pose...", once=True)
+
+        # Select a random direction to move and a random distance.
+        distance = np.random.uniform(0.0, 0.1)
+        angle = np.random.uniform(-np.pi/6, np.pi/6)
+
+        # Update the current pose.
+        self.curr_pose[0] += distance * np.cos(self.curr_pose[2])
+        self.curr_pose[1] += distance * np.sin(self.curr_pose[2])
+        self.curr_pose[2] += angle
+
+        # Prepare and publish ROS message.
+        msg = PoseWithCovarianceStamped()
+
+        msg.header.frame_id = "world"
+        msg.header.stamp = self.get_clock().now().to_msg()
+
+        msg.pose.pose.position.x = self.curr_pose[0]
+        msg.pose.pose.position.y = self.curr_pose[1]
+        msg.pose.pose.position.z = 0.0
+
+        self.publisher.publish(msg)
+
+
+if __name__ == "__main__":
+    rclpy.init(args=None)
+
+    fake_pose_publisher = FakePosePublisher()
+    rclpy.spin(fake_pose_publisher)
+
+    fake_pose_publisher.destroy_node()
+    rclpy.shutdown()
