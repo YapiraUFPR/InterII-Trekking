9f9a3a387696445c5b637e9a8c96db80406bc2d4
Gabp0 <gabriel.pontarolo045@gmail.com>
2024-04-21 12:15:32 -0300

    Use jetson-utils video module for camera publisher.

--- src/drivers/src/camera_publisher.cpp
+++ src/drivers/src/camera_publisher.cpp
@@ -7,8 +7,12 @@
 #include <opencv2/opencv.hpp>
 #include <opencv2/imgcodecs.hpp>
 
+#include <jetson-utils/videoSource.h>
+#include "image_converter.h"
+
 #include "rclcpp/rclcpp.hpp"
 #include "sensor_msgs/msg/compressed_image.hpp"
+#include "sensor_msgs/msg/image.hpp"
 
 class ImxPublisher : public rclcpp::Node
 {
@@ -30,85 +34,95 @@ public:
         fs.release();
 
         // initialize camera
-        std::string gst_pipeline = this->gstreamerPipeline(input_stream, cv::Size(resolution[0], resolution[1]), sample_rate, 0);
-        RCLCPP_INFO(this->get_logger(), "Gstreamer pipeline:\n%s", gst_pipeline.c_str());
-        // cv::VideoCapture cap(gst_pipeline, cv::CAP_GSTREAMER);
-        cv::VideoCapture cap(0);
-        cap.open(input_stream);
-        if (!cap.isOpened())
-        {
-            RCLCPP_ERROR(this->get_logger(), "Could not open camera.");
-        }
+        // std::string gst_pipeline = this->gstreamerPipeline(input_stream, cv::Size(resolution[0], resolution[1]), sample_rate, 0);
+        // RCLCPP_INFO(this->get_logger(), "Gstreamer pipeline:\n%s", gst_pipeline.c_str());
+        // this->cap = cv::VideoCapture(gst_pipeline, cv::CAP_GSTREAMER);
+        // this->cap.open(input_stream);
+        // if (!this->cap.isOpened())
+        // {
+        //     RCLCPP_ERROR(this->get_logger(), "Could not open camera.");
+        // }
+
+        videoOptions video_options;
+
+        video_options.width = 640;
+        video_options.height = 480;
+        video_options.latency = 1;
+
+        this->cap = videoSource::Create("csi://0", video_options);
+        this->image_cvt = new imageConverter();
+
         RCLCPP_INFO(this->get_logger(), "Camera publisher has been started.");
 
         // initialize publisher
-        this->publisher = this->create_publisher<sensor_msgs::msg::CompressedImage>(camera_topic, 10);
+        this->publisher = this->create_publisher<sensor_msgs::msg::Image>(camera_topic, 10);
         this->timer = this->create_wall_timer(std::chrono::milliseconds(1000 / sample_rate), std::bind(&ImxPublisher::publish, this));
     }
 
 private:
     std::string gstreamerPipeline(int sensor_id, cv::Size img_size, int frame_rate, int flip_method)
     {
-        //  "nvarguscamerasrc sensor-id=%d ! "
-        //  "video/x-raw(memory:NVMM), width=(int)%d, height=(int)%d, framerate=(fraction)%d/1 ! "
-        //  "nvvidconv flip-method=%d ! "
-        //  "video/x-raw, width=(int)%d, height=(int)%d, format=(string)BGRx ! "
-        //  "videoconvert ! "
-        //  "video/x-raw, format=(string)BGR ! appsink"
-
-        // 'nvarguscamerasrc sensor-mode={sensor_mode} '
-        // '! video/x-raw(memory:NVMM), width={capture_width}, height={capture_height}, format=(string)NV12, framerate=(fraction){fps}/1 !'
-        // ' nvvidconv '
-        // '! video/x-raw, width=(int){width}, height=(int){height}, format=(string)BGRx !'
-        // ' videoconvert '
-        // '! video/x-raw, format=(string)BGR !'
-        // ' appsink name=sink
-
-        const char *fmt = "nvarguscamerasrc \
-            ! video/x-raw(memory:NVMM), width=1280, height=720, framerate=%d/1, format=NV12 \
+        const char *fmt = "nvarguscamerasrc sensor-id=%d \
+            ! video/x-raw(memory:NVMM), width=(int)%d, height=(int)%d, framerate=%d/1, format=(string)NV12 \
             ! nvvidconv flip-method=%d \
-            ! video/x-raw, width=%d, height=%d, format=BGRx \
-            ! videoconvert \
-            ! video/x-raw, format=BGR \
-            ! appsink drop=1";
+            ! video/x-raw(memory:NVMM) \
+            ! ! nvegltransform ! nveglglessink -e";
 
         int sz = std::snprintf(nullptr, 0, fmt, std::sqrt(2));
         std::vector<char> buf(sz + 1); // note +1 for null terminator
         std::sprintf(buf.data(), fmt, 
-             frame_rate, flip_method, img_size.width, img_size.height);
+             sensor_id, img_size.width, img_size.height, frame_rate, flip_method);
 
         return std::string(buf.data());
-
-        // return "nvarguscamerasrc sensor_id=" + std::to_string(sensor_id) + 
-        //     " ! video/x-raw(memory:NVMM), width=(int)" + std::to_string(img_size.width) + ", height=(int)" + std::to_string(img_size.height) + 
-        //     ", framerate=(fraction)" + std::to_string(frame_rate) + "/1 ! nvvidconv flip-method=" + std::to_string(flip_method) + " ! video/x-raw, width=(int)" + std::to_string(img_size.width) + ", height=(int)" + std::to_string(img_size.height) + ", format=(string)BGRx, ! videoconvert ! video/x-raw, format=(string)BGR ! appsink";
     }
 
     void publish(void)
     {
-        cv::Mat frame;
-        std::vector<uchar> buffer;
-        sensor_msgs::msg::CompressedImage msg;
+        // cv::Mat frame;
+        // std::vector<uchar> buffer;
+
+        sensor_msgs::msg::Image msg;
+        imageConverter::PixelType* nextFrame = NULL;
+
+        // get the latest frame
+        if(!this->cap->Capture(&nextFrame, 1000) )
+        {
+            ROS_ERROR("failed to capture next frame");
+            return;
+        }
+
+        // assure correct image size
+        if(!this->image_cvt->Resize(this->cap->GetWidth(), this->cap->GetHeight(), imageConverter::ROSOutputFormat))
+        {
+            ROS_ERROR("failed to resize camera image converter");
+            return;
+        }
+
+        // convert the image to ROS format
+        this->image_cvt->Convert(msg, imageConverter::ROSOutputFormat, nextFrame);
+
 
         // get frame from camera
-        cap >> frame;
-        if (frame.empty())
-        {
-            RCLCPP_ERROR(this->get_logger(), "Could not get frame");
-        }
+        // cap >> frame;
+        // if (frame.empty())
+        // {
+        //     RCLCPP_ERROR(this->get_logger(), "Could not get frame");
+        // }
         
         RCLCPP_INFO_ONCE(this->get_logger(), "Publishing camera frames...");
 
         // convert and publish frame
         msg.header.stamp = this->now();
-        msg.format = "jpeg";
-        cv::imencode(".jpeg", frame, buffer);
-        msg.data = buffer;
+        // msg.format = "jpeg";
+        // cv::imencode(".jpeg", frame, buffer);
+        // msg.data = buffer;
         publisher->publish(msg);
     }
 
-    cv::VideoCapture cap;
-    rclcpp::Publisher<sensor_msgs::msg::CompressedImage>::SharedPtr publisher;
+    videoSource* cap; 
+    imageConverter* image_cvt;
+    // cv::VideoCapture cap;
+    rclcpp::Publisher<sensor_msgs::msg::Image>::SharedPtr publisher;
     rclcpp::TimerBase::SharedPtr timer;
 };
 
--- src/visualizer/src/visualization_node.cpp
+++ src/visualizer/src/visualization_node.cpp
@@ -5,7 +5,8 @@
 
 #include <opencv2/opencv.hpp>
 
-#include <geometry_msgs/msg/pose_stamped.hpp>
+#include <geometry_msgs/msg/pose_with_covariance_stamped.hpp>
+#include <nav_msgs/msg/odometry.hpp>
 #include <visualization_msgs/msg/marker.hpp>
 
 const std::vector<std::vector<float>> COLORS = {
@@ -41,9 +42,9 @@ public:
             msg.type = visualization_msgs::msg::Marker::LINE_STRIP; 
             msg.pose.orientation.w = 1.0;
 
-            msg.scale.x = 0.05; 
-            msg.scale.y = 0.05;
-            msg.scale.z = 0.05;
+            msg.scale.x = 1.05; 
+            msg.scale.y = 1.05;
+            msg.scale.z = 1.05;
 
             msg.color.r = COLORS[i][0];
             msg.color.g = COLORS[i][1];
@@ -56,8 +57,8 @@ public:
         // Initialize subscribers
         for (size_t i = 0; i < visualization_topics.size(); i++)
         {
-            std::function<void(const geometry_msgs::msg::PoseStamped::SharedPtr msg)> callback = std::bind(&VisualizerNode::pose_callback, this, std::placeholders::_1, i);
-            this->pose_subscribers.push_back(this->create_subscription<geometry_msgs::msg::PoseStamped>(visualization_topics[i], 10, callback));
+            std::function<void(const nav_msgs::msg::Odometry::SharedPtr msg)> callback = std::bind(&VisualizerNode::pose_callback, this, std::placeholders::_1, i);
+            this->pose_subscribers.push_back(this->create_subscription<nav_msgs::msg::Odometry>(visualization_topics[i], 10, callback));
 
             RCLCPP_INFO(this->get_logger(), ("/visualization" + visualization_topics[i]).c_str());
             this->markers_publishers.push_back(this->create_publisher<visualization_msgs::msg::Marker>("/visualization" + visualization_topics[i], 10));
@@ -68,17 +69,17 @@ public:
 
 private:
     
-    void pose_callback(const geometry_msgs::msg::PoseStamped::SharedPtr msg, int idx)
+    void pose_callback(const nav_msgs::msg::Odometry::SharedPtr msg, int idx)
     {
         RCLCPP_INFO_ONCE(this->get_logger(), "Started publishing markers...");
 
         this->msgs[idx].header.stamp = this->now();
-        this->msgs[idx].points.push_back(msg->pose.position);
+        this->msgs[idx].points.push_back(msg->pose.pose.position);
         this->markers_publishers[idx]->publish(this->msgs[idx]);
     }
 
     std::vector<visualization_msgs::msg::Marker> msgs;
-    std::vector<rclcpp::Subscription<geometry_msgs::msg::PoseStamped>::SharedPtr> pose_subscribers;
+    std::vector<rclcpp::Subscription<nav_msgs::msg::Odometry>::SharedPtr> pose_subscribers;
     std::vector<rclcpp::Publisher<visualization_msgs::msg::Marker>::SharedPtr> markers_publishers;
 };
 
