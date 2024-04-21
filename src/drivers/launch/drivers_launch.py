271eb1fdc4093271bdc0910b73728f2acb8149e0
Gabp0 <gabriel.pontarolo045@gmail.com>
2024-04-21 20:18:17 -0300

    Add conditions to drivers launch.

--- src/drivers/launch/drivers_launch.py
+++ src/drivers/launch/drivers_launch.py
@@ -1,34 +1,74 @@
 from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, LogInfo, OpaqueFunction
+from launch.conditions import IfCondition
+from launch.substitutions import LaunchConfiguration, TextSubstitution
 from launch_ros.actions import Node
 
-def generate_launch_description():
-    return LaunchDescription([
+launch_args = [
+    DeclareLaunchArgument(
+        name="camera_enable", default_value="true", description="enable camera node"
+    ),
+    DeclareLaunchArgument(
+        name="imu_enable", default_value="true", description="enable IMU node"
+    ),
+    DeclareLaunchArgument(
+        name="status_led_enable", default_value="false", description="enable led node"
+    ),
+    DeclareLaunchArgument(
+        name="dist_sensor_enable", default_value="false", description="enable distance sensor node"
+    ),
+    DeclareLaunchArgument(
+        name="bat_monitor_enable", default_value="false", description="enable battery monitor node"
+    ),
+    DeclareLaunchArgument(
+        name="color_sensor_enable", default_value="false", description="enable color sensor node"
+    ),
+    DeclareLaunchArgument(
+        name="motor_enable", default_value="false", description="enable motor node"
+    ),
+]
+
+def launch_setup(context):
+    return [
         Node(
             package='drivers',
+            condition=IfCondition(LaunchConfiguration("camera_enable")),
             executable='camera_publisher',
         ),
         Node(
             package='drivers',
+            condition=IfCondition(LaunchConfiguration("status_led_enable")),
             executable='led_listener',
         ),
         Node(
             package='drivers',
+            condition=IfCondition(LaunchConfiguration("imu_enable")),
             executable='bno_publisher.py',
         ),
         Node(
             package='drivers',
+            condition=IfCondition(LaunchConfiguration("dist_sensor_enable")),
             executable='vl5_publisher.py',
         ),
         Node(
             package='drivers',
+            condition=IfCondition(LaunchConfiguration("bat_monitor_enable")),
             executable='ina_publisher.py',
         ),
         Node(
             package='drivers',
+            condition=IfCondition(LaunchConfiguration("color_sensor_enable")),
             executable='tcs32_publisher.py',
         ),
-        # Node(
-        #     package='drivers',
-        #     executable='motor_listener',
-        # ),
-    ])
+        Node(
+            package='drivers',
+            condition=IfCondition(LaunchConfiguration("motor_enable")),
+            executable='motor_listener',
+        ),
+    ]
+
+def generate_launch_description():
+    opfunc = OpaqueFunction(function=launch_setup)
+    ld = LaunchDescription(launch_args)
+    ld.add_action(opfunc)
+    return ld
